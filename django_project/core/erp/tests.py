23e62a4426f97e5ff21cd120afbd4d2bdf511f41
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-08-07 17:56:38 -0500

    tests con exito

--- django_project/core/erp/tests.py
+++ django_project/core/erp/tests.py
@@ -1,6 +1,17 @@
-import os 
-
-
 from core.erp.models import Type
+
+
+new_type = Type()
+new_type.name = "Gerente"
+new_type.save()
+
+
+# listar
+
+# select * from table
+Type.objects.all()
+
+
+
 
 
