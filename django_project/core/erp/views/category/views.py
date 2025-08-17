06eda8aaa67acd6bea0481efd146772911823920
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-08-16 19:42:40 -0500

    Editando listas de categorias

--- django_project/core/erp/views/category/views.py
+++ django_project/core/erp/views/category/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 
 def category_list(request):
     data = {
-
+        'title': 'Listado de Categorias'
     }
     return render(request, 'category/list.html', data)
---
+++ django_project/urls.py
@@ -0,0 +1,8 @@
+# urls.py (del proyecto principal)
+from django.contrib import admin
+from django.urls import path, include
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('erp/', include('core.urls')),
+]
