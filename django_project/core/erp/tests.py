66fd6aca9d2eaa0b918b2c91a204ed61ac49162c
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-08-19 15:40:59 -0500

    Integrando datatables

--- django_project/core/erp/tests.py
+++ django_project/core/erp/tests.py
@@ -1,16 +1,17 @@
 
-from core.erp.models import Type
+from core.erp.models import *
+
+data = ['Leche y derivados', 'Carnes, pescados y huevos', 'Patatas, legumbres, frutos secos',
+        'Verduras y Hortalizas', 'Frutas', 'Cereales y derivados, azúcar y dulces',
+        'Grasas, aceite y mantequilla']
+
+for i in data:
+    cat = Category(name=i)
+    cat.save()
+    print('Guardado registro N°{}'.format(cat.id))
 
 
-new_type = Type()
-new_type.name = "Gerente"
-new_type.save()
 
-
-# listar
-
-# select * from table
-Type.objects.all()
 
 
 
