dfa9679051dd6f1b621b39315f266bb577dc886a
Norbey Gutierrez <norbeyd_gutierrez@soy.sena.edu.co>
2025-08-19 18:33:45 -0500

    datatables& C:/Users/gutie/OneDrive/Escritorio/PROYECTO_DJANGO/venv/Scripts/Activate.ps1

--- django_project/core/erp/views/category/views.py
+++ django_project/core/erp/views/category/views.py
@@ -1,6 +1,10 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.views.decorators.csrf import csrf_exempt
+from django.contrib.auth.decorators import login_required
+from django.http import JsonResponse
 from core.erp.models import *
 from django.views.generic import ListView
+from django.utils.decorators import method_decorator
 
 
 def category_list(request):
@@ -15,8 +19,16 @@ class CategoryListView(ListView):
     model = Category
     template_name = 'category/list.html'
 
-    def get_queryset(self):
-        return Category.objects.all()
+    @method_decorator(csrf_exempt)
+    def dispatch(self, request, *args, **kwargs):
+        return super().dispatch(request, *args, **kwargs)
+
+    
+    def post(self, request, *args, **kwargs):
+        data = {
+            'name': 'Norbey',
+        }
+        return JsonResponse(data)
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
--- django_project/static/lib/datatables/datatables.js
+++ django_project/static/lib/datatables/datatables.js
@@ -4,7 +4,7 @@
  *
  * To rebuild or modify this file with the latest versions of the included
  * software please visit:
- *   https://datatables.net/download/#bs4/dt-2.3.2
+ *   https://datatables.net/download/#dt/dt-2.3.2
  *
  * Included libraries:
  *   DataTables 2.3.2
@@ -513,7 +513,7 @@
 		 *
 		 *  @type string
 		 */
-		builder: "bs4/dt-2.3.2",
+		builder: "dt/dt-2.3.2",
 	
 		/**
 		 * Buttons. For use with the Buttons extension for DataTables. This is
@@ -14085,7 +14085,7 @@
 }));
 
 
-/*! DataTables Bootstrap 4 integration
+/*! DataTables styling integration
  * © SpryMedia Ltd - datatables.net/license
  */
 
@@ -14136,72 +14136,6 @@ var DataTable = $.fn.dataTable;
 
 
 
-/**
- * DataTables integration for Bootstrap 4.
- *
- * This file sets the defaults and adds options to DataTables to style its
- * controls using Bootstrap. See https://datatables.net/manual/styling/bootstrap
- * for further information.
- */
-
-/* Set the defaults for DataTables initialisation */
-$.extend( true, DataTable.defaults, {
-	renderer: 'bootstrap'
-} );
-
-
-/* Default class modification */
-$.extend( true, DataTable.ext.classes, {
-	container: "dt-container dt-bootstrap4",
-	search: {
-		input: "form-control form-control-sm"
-	},
-	length: {
-		select: "custom-select custom-select-sm form-control form-control-sm"
-	},
-	processing: {
-		container: "dt-processing card"
-	},
-	layout: {
-		row: 'row justify-content-between',
-		cell: 'd-md-flex justify-content-between align-items-center',
-		tableCell: 'col-12',
-		start: 'dt-layout-start col-md-auto mr-auto',
-		end: 'dt-layout-end col-md-auto ml-auto',
-		full: 'dt-layout-full col-md'
-	}
-} );
-
-
-/* Bootstrap paging button renderer */
-DataTable.ext.renderer.pagingButton.bootstrap = function (settings, buttonType, content, active, disabled) {
-	var btnClasses = ['dt-paging-button', 'page-item'];
-
-	if (active) {
-		btnClasses.push('active');
-	}
-
-	if (disabled) {
-		btnClasses.push('disabled')
-	}
-
-	var li = $('<li>').addClass(btnClasses.join(' '));
-	var a = $('<a>', {
-		'href': disabled ? null : '#',
-		'class': 'page-link'
-	})
-		.html(content)
-		.appendTo(li);
-
-	return {
-		display: li,
-		clicker: a
-	};
-};
-
-DataTable.ext.renderer.pagingContainer.bootstrap = function (settings, buttonEls) {
-	return $('<ul/>').addClass('pagination').append(buttonEls);
-};
 
 
 return DataTable;
